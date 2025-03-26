21e448dbaa6cba9830ee57822f82683343754f00
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-26 06:07:44 -0600

    Creacion de la clase BinaryTree

---
+++ BinaryTree.java
@@ -0,0 +1,13 @@
+/*
+ * Implementación de la clase BinaryTree del libro Java Structures: Data Structures for the Principled Programmer
+ * Link: https://www.cs.williams.edu/~bailey/JavaStructures/Book_files/JavaStructures.pdf
+ * en la pág: 284 The BinaryTreeImplementation
+ */
+
+public class BinaryTree<E> {
+
+    public BinaryTree() {
+
+    }
+
+}
