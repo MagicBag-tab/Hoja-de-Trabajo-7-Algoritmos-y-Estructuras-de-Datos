5347150a00c62c625d49cb0d1193906002368322
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-26 06:32:06 -0600

    Constructor de products y sus setters y getters

--- Products.java
+++ Products.java
@@ -1,3 +1,50 @@
+/*
+ * Clase de products donde van todos los elementos de los productos.
+ */
+
 public class Products {
+    private int SKU;
+    private String name;
+    private String description;
+    private String cantpersize;
+
+    public Products(int SKU, String name, String description, String cantpersize) {
+        this.SKU = SKU;
+        this.name = name;
+        this.description = description;
+        this.cantpersize = cantpersize;
+    }
+
+    public int getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(int sKU) {
+        SKU = sKU;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCantpersize() {
+        return cantpersize;
+    }
+
+    public void setCantpersize(String cantpersize) {
+        this.cantpersize = cantpersize;
+    }
 
 }
